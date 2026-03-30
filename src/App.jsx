e230c0a74bc641b665a885bfb66d7d5228b41b18
mdihbinah <mdihbinah@gmail.com>
2026-03-30 09:33:16 +0600

    SetUp Complete and Start Assignment.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl bg-amber-300'>Hello</h1>
+      <button className='btn btn-accent'>How are you?</button>
+    </>
+  )
+}
+
+export default App
